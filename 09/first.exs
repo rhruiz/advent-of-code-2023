694746aebf67e11ecfa257d5e03b14cd34253372
Ricardo Hermida Ruiz <ricardo.ruiz@locaweb.com.br>
2023-12-10 14:37:49 -0300

    Day 09, first star

---
+++ 09/first.exs
@@ -0,0 +1,30 @@
+defmodule OASIS do
+  def predict(list) do
+    lasts = predict(list, [])
+
+    Enum.reduce(lasts, List.last(list), fn last, acc -> last + acc end)
+  end
+
+  defp predict(list, acc) do
+    {all_zeros, diffs, last} =
+      list
+      |> Stream.zip(Stream.drop(list, 1))
+      |> Stream.map(fn {x, y} -> y - x end)
+      |> Enum.reduce({true, [], nil}, fn diff, {all_zeros, diffs, _last} ->
+        {all_zeros && (diff == 0), [diff | diffs], diff}
+      end)
+
+    case all_zeros do
+      true -> acc
+      false -> predict(Enum.reverse(diffs), [last | acc])
+    end
+  end
+end
+
+IO.stream(:stdio, :line)
+|> Stream.map(&String.trim/1)
+|> Stream.map(&String.split(&1, " "))
+|> Stream.map(fn line -> Enum.map(line, &String.to_integer/1) end)
+|> Stream.map(&OASIS.predict/1)
+|> Enum.reduce(0, &+/2)
+|> IO.inspect()
