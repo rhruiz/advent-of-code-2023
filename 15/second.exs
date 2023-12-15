a3e7cd94ed3e052568de8bb7a1728f4cafdb1ac5
Ricardo Hermida Ruiz <ricardo.ruiz@locaweb.com.br>
2023-12-15 16:38:10 -0300

    Refactor

--- 15/second.exs
+++ 15/second.exs
@@ -6,55 +6,65 @@ defmodule HASH do
       (acc + chr) |> Kernel.*(17) |> rem(256)
     end)
   end
+end
 
-  def operation(str) do
-    if String.ends_with?(str, "-") do
-      {:remove, String.slice(str, 0..-2)}
-    else
-      [label, focal_length] = String.split(str, "=")
-      {:set, label, String.to_integer(focal_length)}
+defmodule HASHMAP do
+  def new() do
+    Enum.into(0..255, %{}, fn key -> {key, {MapSet.new(), []}} end)
+  end
+
+  def focusing_power(hashmap) do
+    for {box, {_set, list}} <- hashmap,
+        {lens, index} <- Enum.with_index(list),
+        reduce: 0 do
+      power -> power + focusing_power(box, index, lens)
     end
   end
 
-  def focusing_power(box, index, {_label, focal_length}) do
+  defp focusing_power(box, index, {_label, focal_length}) do
     (box + 1) * (index + 1) * focal_length
+  end
+
+  def delete(hashmap, label) do
+    Map.update!(hashmap, HASH.hash(label), fn {set, list} ->
+      if label in set do
+        {MapSet.delete(set, label), Enum.reject(list, &match?({^label, _}, &1))}
+      else
+        {set, list}
+      end
+    end)
+  end
+
+  def set(hashmap, label, focal_length) do
+    Map.update!(hashmap, HASH.hash(label), fn {set, list} ->
+      if label in set do
+        {set,
+         Enum.map(list, fn
+           {^label, _} -> {label, focal_length}
+           item -> item
+         end)}
+      else
+        {MapSet.put(set, label), list ++ [{label, focal_length}]}
+      end
+    end)
+  end
+
+  def put(str, hashmap) when is_binary(str) and is_map(hashmap) do
+    put(hashmap, str)
+  end
+
+  def put(hashmap, str) do
+    if String.ends_with?(str, "-") do
+      delete(hashmap, String.slice(str, 0..-2))
+    else
+      [label, focal_length] = String.split(str, "=")
+      set(hashmap, label, String.to_integer(focal_length))
+    end
   end
 end
 
-buckets = Enum.into(0..255, %{}, fn key -> {key, {MapSet.new(), []}} end)
-
 IO.stream(:stdio, :line)
 |> Stream.flat_map(&String.split(&1, ","))
-|> Enum.reduce(buckets, fn str, buckets ->
-  case HASH.operation(str) do
-    {:remove, label} ->
-      Map.update!(buckets, HASH.hash(label), fn {set, list} ->
-        if label in set do
-          {MapSet.delete(set, label), Enum.reject(list, &match?({^label, _}, &1))}
-        else
-          {set, list}
-        end
-      end)
-
-    {:set, label, focal_length} ->
-      Map.update!(buckets, HASH.hash(label), fn {set, list} ->
-        if label in set do
-          {set,
-           Enum.map(list, fn
-             {^label, _} -> {label, focal_length}
-             item -> item
-           end)}
-        else
-          {MapSet.put(set, label), list ++ [{label, focal_length}]}
-        end
-      end)
-  end
-end)
-|> Enum.reduce(0, fn {box, {_set, list}}, acc ->
-  list
-  |> Enum.with_index()
-  |> Enum.reduce(acc, fn {lens, index}, acc ->
-    acc + HASH.focusing_power(box, index, lens)
-  end)
-end)
+|> Enum.reduce(HASHMAP.new(), &HASHMAP.put/2)
+|> HASHMAP.focusing_power()
 |> IO.inspect()
